8bf4d8cd0951a0165cfd76237d18d6f1eb82d508
yannxin <yannxin@qq.com>
2013-09-02 16:37:20 +0800

    Common used algorithms about search and sort

---
+++ QuickSort/QuickSort.cpp
@@ -0,0 +1,65 @@
+// QuickSort.cpp : 
+//
+
+#include "stdafx.h"
+#include "QuickSort.h"
+
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*
+
+int partition(int data[], int low, int high)
+{
+	int key = data[high];
+	int i = low - 1;
+	int j = low;
+	for (j = low; j < high; j++)
+	{
+		if (data[j] <= key)   //must add "="
+		{
+			i++;
+			swap(&data[i], &data[j]);
+		}
+	}
+	i++;
+	swap(&data[i], &data[high]);
+	return i;
+}
+*/
+
+
+//Houre
+int partition(int data[], int low, int high)
+{
+	int key = data[low];
+	int i = low;
+	int j = high;
+	for (j = low; j < high; j++)
+	{
+		if (data[j] <= key)   //must add "="
+		{
+			i++;
+			swap(&data[i], &data[j]);
+		}
+	}
+	i++;
+	swap(&data[i], &data[high]);
+	return i;
+}
+
+
+void quickSort(int data[], int low, int high)
+{
+	if (low < high)
+	{
+		int p = partition(data, low, high);
+		quickSort(data, low, p-1);
+		quickSort(data, p+1, high);
+	}
+}
+
---
+++ QuickSort/QuickSort.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void swap(int* a, int* b);
+int partition(int data[], int low, int high);
+void quickSort(int data[], int low, int high);
